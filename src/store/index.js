7e1739a08bbfcc72117f608673ac1b4fb907de95
Gabriel Enrique <gabrielgeraldo297@gmail.com>
2022-10-24 00:42:08 -0300

    Add Vuex, Vue router and Axios

---
+++ src/store/index.js
@@ -0,0 +1,57 @@
+import { createStore } from "vuex";
+import { SET_USER, LOGOUT_USER } from "./mutations";
+import http from "@/http";
+
+export const store = createStore({
+    state: {
+        user: {
+            id: 0,
+            name: "",
+            email: "",
+            photo_path: "",
+            createdAt: "",
+            token: ""
+        },
+
+        mutations: {
+            [SET_USER] (state, { id, name, email, photo_path, createdAt, token }) {
+                state.user.id = id,
+                state.user.name = name,
+                state.user.email = email,
+                state.user.photo_path = photo_path,
+                state.user.createdAt = createdAt,
+                state.user.token = token
+            },
+
+            [LOGOUT_USER] (state) {
+                state.user.id = 0,
+                state.user.name = "",
+                state.user.email = "",
+                state.user.photo_path = "",
+                state.user.createdAt = "",
+                state.user.token = ""
+            }
+        },
+
+        actions: {
+            signIn({ commit }, user) {
+                return new Promise((resolve, reject) => {
+                    http.post("/api/user/authenticate", user)
+                        .then(response => {
+                            commit(SET_USER, {
+                                id: response.id,
+                                name: response.name,
+                                email: response.email,
+                                photo_path: response.photo_path,
+                                createdAt: response.createdAt,
+                                token: response.token
+                            });
+
+                            resolve(response)
+                        })
+                        .catch(error => reject(error))
+                });
+            }
+        }
+    }
+});
